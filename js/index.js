17dd622fd97e6f1582a384bb020d22f86bd56bce
Kumari Anushka <63368506+anuxoxo@users.noreply.github.com>
2021-06-09 01:17:05 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,18 @@
+function checkTime(num) {
+  if (num < 10) {
+    return "0" + num;
+  } else {
+    return num;
+  }
+}
+
 function displayTime() {
   let today = new Date();
   let timePara = document.getElementById("time");
-  let timeStr =
-    today.getHours() + " : " + today.getMinutes() + " : " + today.getSeconds();
+  let hr = checkTime(today.getHours());
+  let min = checkTime(today.getMinutes());
+  let sec = checkTime(today.getSeconds());
+  let timeStr = hr + " : " + min + " : " + sec;
 
   timePara.innerHTML = timeStr;
 }
